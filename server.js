35e2492988cc845dbd757caa2cdadd26e20c96d8
Mahendra-Yadagiri <mahendrayadagiri45@gmail.com>
2025-11-27 22:36:52 +0530

    changes are done1234

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req,res) =>{
+app.get("/*", (req,res) =>{
 res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
